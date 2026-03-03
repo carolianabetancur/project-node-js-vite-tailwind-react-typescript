8c118734e11b229c3caab8c3d44051f390b746c0
carolina-perez-simetrik <carolina.perez@simetrik.com>
2026-03-03 13:04:15 -0500

    Created styles theme with tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
+      {/* <div>
+        <a href="https://vite.dev" target="_blank" rel="noopener noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noopener noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button type="button" onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
@@ -27,9 +27,44 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
+      <div className="App p-12">
+        <h1 className="tracking-wide text-gray-600 text-3xl font-bold">
+          Custom Utility Colors Using Tailwind `extend`
+        </h1>
+        <div className="flex flex-wrap gap-4 mx-auto w-full flex justify-center p-12 rounded-md">
+          <div className="flex items-center justify-center text-gray-800 bg-primary-100 w-40 h-40 rounded-md shadow-xl">
+            Lemon Yellow
+          </div>
+          <div className="flex items-center justify-center text-gray-100 bg-secondary-100 w-40 h-40 rounded-md shadow-xl">
+            Rose Pink
+          </div>
+          <div className="flex items-center justify-center text-gray-100 bg-accent-100 w-40 h-40 rounded-md shadow-xl">
+            Blood Red
+          </div>
+          <div className="flex items-center justify-center text-gray-700 bg-neutralSoft-100 w-40 h-40 rounded-md shadow-xl">
+            Parrot Feather
+          </div>
+          <div className="flex items-center justify-center text-gray-100 bg-backgroundSoft-100 w-40 h-40 rounded-md shadow-xl">
+            Purple Extended
+          </div>
+        </div>
+      </div>
+      <div className="App p-12">
+        <h1 className="tracking-wide text-gray-600 text-3xl font-bold">
+          Custom Utility Colors Using Tailwind `extend`
+        </h1>
+        <div className="mt-12 flex justify-center gap-12">
+          <div>
+            <button type="button" className="btn-primary">
+              Small Button
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="card w-20 h-20">Card</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,52 @@
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: {
+					50: "#fff1ef",
+					100: "#efb0a9",
+					200: "#e39b93",
+					300: "#d7867e",
+				},
+				secondary: {
+					50: "#f3e8e9",
+					100: "#bda0a2",
+					200: "#a88b8d",
+					300: "#937679",
+				},
+				accent: {
+					50: "#f2fbfd",
+					100: "#d1eaee",
+					200: "#b8d7dc",
+					300: "#9fc4ca",
+				},
+				neutralSoft: {
+					50: "#f2f1ea",
+					100: "#cbc8b5",
+					200: "#b5b29f",
+					300: "#9f9c89",
+				},
+				backgroundSoft: {
+					50: "#fff8f4",
+					100: "#ffe6db",
+				},
+			},
+			fontFamily: {
+				sans: ["Inter", "sans-serif"],
+				serif: ["Merriweather", "serif"],
+			},
+			spacing: {
+				128: "32rem",
+				144: "36rem",
+			},
+			borderRadius: {
+				"4xl": "2rem",
+			},
+			boxShadow: {
+				soft: "0 4px 20px rgba(0,0,0,0.05)",
+			},
+		},
 	},
 	plugins: [],
 };
